2cd99d7e952ab4621baf40b3983a149cef4af3f3
Ben M <ben@mosesweb.co.uk>
2025-07-14 22:09:48 +0100

    feat: Rewrite Cracking RSA using the extended Euclidean algorithm, including proof. Runs almost instantly.

--- number-theory/easy/crackingrsa.cpp
+++ number-theory/easy/crackingrsa.cpp
@@ -1,49 +1,89 @@
-#include <cstdio>
-#include <vector>
 #include <cmath>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
 bool isPrime(int num) {
-    for (int m = 2; m <= sqrt(num); m++) {
-        if (num % m == 0) {
-            return false;
-        }
+  int squirt = sqrt(num);
+  for (int m = 2; m <= squirt; m++) {
+    if (num % m == 0) {
+      return false;
     }
-    return true;
+  }
+  return true;
+}
+
+// EEA to find gcd and Bezout coefficients
+// gcd(a, b) = gcd(b, a % b)
+long long extendedGCD(long long a, long long b, long long &x, long long &y) {
+  if (b == 0) {
+    // gcd(a,0) = a
+    // a * x + 0 * y = a
+    // a * 1 + 0 * 0 = a
+    x = 1;
+    y = 0;
+    return a;
+  }
+
+  // Recurse until the remainder is 0 - found the gcd
+  long long x1, y1;
+  long long gcd = extendedGCD(b, a % b, x1, y1);
+
+  // Return calls - update x and y and return up the stack
+  // gcd(b, a % b) = (b * x) + ((a % b) * y) = gcd(a, b)
+  // a % b = a - (a / b) * b
+  // (b * x) + ((a - (a / b) * b) * y) = gcd(a, b)
+  // (b * x) + (a * y) - ([a / b] * b * y)
+  // (a * y) + b * (x) - b * ([a / b] * y)
+  // a * y + b * (x - [a / b] * y)
+  // Therefore:
+  x = y1;
+  y = x1 - (a / b) * y1;
+
+  return gcd;
+}
+
+long long modInverse(long long b, long long m) {
+  // Find x where (a * x) - (b * y) = gcd(a, b)
+  long long x, y;
+  long long gcd = extendedGCD(b, m, x, y);
+
+  if (gcd != 1) {
+    return -1; // Inverse doesn't exist if base and modulus are not coprime
+  }
+
+  // Handle negative x
+  return (x % m + m) % m;
 }
 
 int main() {
-    vector<int> primes;
-    
-    // Initialise prime numbers
-    for (int i = 2; i <= 1000; i++) {
-        if (isPrime(i)) {
-            primes.push_back(i);
-        }
+  vector<int> primes;
+
+  // Initialise prime numbers
+  for (int i = 2; i <= 1000; i++) {
+    if (isPrime(i)) {
+      primes.push_back(i);
     }
-    
-    int cases;
-    scanf("%d", &cases);
-    
-    for (int j = 0; j < cases; j++) {
-        long n, e;
-        scanf("%ld %ld", &n, &e);
-        int p, q;
-        long eulerN;
-        for (int k = 0; k < primes.size() - 1; k++) {
-            if (n % primes[k] == 0) {
-                p = primes[k];
-                q = n / p;
-                break;
-            }
-        }
-        eulerN = (p - 1) * (q - 1);
-        int d;
-        for (int x = 1; x <= eulerN; x++) {
-            if ((e * x) % eulerN == 1) {
-                printf("%d\n", x);
-            }
-        }
+  }
+
+  int cases;
+  cin >> cases;
+
+  for (int j = 0; j < cases; j++) {
+    long n, e;
+    cin >> n >> e;
+    int p, q;
+    long long eulerN;
+    for (int k = 0; k < primes.size() - 1; k++) {
+      if (n % primes[k] == 0) {
+        p = primes[k];
+        q = n / p;
+        break;
+      }
     }
+    eulerN = (p - 1) * (q - 1);
+    long long d = modInverse(e, eulerN);
+    cout << d << "\n";
+  }
 }
