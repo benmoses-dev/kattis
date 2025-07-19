f6b0597175d6118c226c28742300f063aca035f7
Ben M <ben@mosesweb.co.uk>
2025-07-19 17:53:16 +0100

    feat: Track distances for undirected graphs

--- snippets/bfs.cpp
+++ snippets/bfs.cpp
@@ -3,31 +3,38 @@
 
 using namespace std;
 
+/**
+ * Generic BFS algorithm for an undirected graph.
+ * Tracks whether the graph has cycles or is bipartite, components, and shortest paths.
+ */
 bool undirectedBfs(vector<vector<int>> &adj) {
     int n = adj.size();
+    int start = 0;
 
     // Track state
     bool hasCycle = false;
     bool isBipartite = true;
     vector<int> parent(n, -1);
     vector<int> colours(n, -1);
+    vector<int> distances(n, -1);
 
     // Assign component IDs
     int componentId = 0;
     vector<int> components(n, -1);
 
-    for (int i = 0; i < n; i++) {
+    for (int i = start; i < n; i++) {
         if (colours[i] != -1)
             continue;
 
         queue<int> q;
         q.push(i);
         colours[i] = 0;
+        components[i] = componentId;
+        distances[i] = 0;
 
         while (!q.empty()) {
             int next = q.front();
             q.pop();
-            components[next] = componentId;
 
             /**
              * Start a BFS through all neighbours.
@@ -36,6 +43,8 @@ bool undirectedBfs(vector<vector<int>> &adj) {
             for (const int &neighbour : adj[next]) {
                 if (colours[neighbour] == -1) {
                     colours[neighbour] = colours[next] ^ 1;
+                    components[neighbour] = componentId;
+                    distances[neighbour] = distances[next] + 1;
                     parent[neighbour] = next;
                     q.push(neighbour);
                 } else {
